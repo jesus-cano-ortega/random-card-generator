f1c4cfa60330d93fd1af49df33bc915bef00e987
Jesús Cano <jcanort@gmail.com>
2021-11-18 20:12:38 +0000

    Creating random card project

---
+++ README.md
@@ -0,0 +1,25 @@
+# <img src="https://github.com/jesus-cano-ortega/js-introduction-exercises/blob/main/assets/resources/img/face.png" width="45" alt="Personal Logo"> Random Card Generator
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://tan-wildebeest-sjz9lphu.ws-eu18.gitpod.io/)
+
+The objective of this project is to display a random card from a deck of poker on the screen using a sequence of JavaScript functions that modifies the DOM.
+To add a little more difficulty, we have decided that each of the buttons will offer to the user a certain type of card, one a simply random car, the other option showing
+a card without repeating the suit, and the last one showing a unique card without repeating until the deck is over.
+
+### Exercises 
+
+Given an index.html document, the app.js file must modify the DOM in order to:
+
+1. Create a function that show a random card (the suit and the number can be repeated)
+2. Create a function that show a random card without repeating the previous suit
+3. Create a function that show a unique random card without repeating
+
+##### REMEMBER: 
+
+Once the progress and flow of the project is finished, you must commit and push the repository remotely, using the following command:
+
+```
+$ git add . 
+$ git commit -m "Message"
+$ git push origin repo-remote
+```
---
+++ src/app.js
@@ -0,0 +1,86 @@
+import "bootstrap";
+import "./style.css";
+
+//Card Container
+const cardNumber = document.querySelector("#cardNumber");
+//DOM Buttons
+const randomCard = document.querySelector("#randomCard");
+const differentSuit = document.querySelector("#differentSuit");
+const noRepeat = document.querySelector("#noRepeat");
+
+var value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+var suit = ["suitspades", "suitdiamonds", "suitclubs", "suithearts"];
+
+//Function that creates a new deck using value and suit variables
+function createDeck() {
+  let newDeck = [];
+
+  for (let i in suit) {
+    for (let j in value) {
+      let card = { value: `${value[j]}`, suit: `${suit[i]}`, used: false };
+      newDeck.push(card);
+    }
+  }
+  return newDeck;
+}
+
+let deck = createDeck();
+console.log(deck);
+
+//Function that shows a random card inside the created deck
+function randomNumber() {
+  return deck[Math.floor(Math.random() * 51)];
+}
+
+//Function that removes the class suit from the selected card
+function removeClass() {
+  cardNumber.classList.remove(
+    "suitspades",
+    "suithearts",
+    "suitclubs",
+    "suitdiamonds"
+  );
+}
+
+//Create a function that show a random card (the suit and the number can be repeated)
+randomCard.addEventListener("click", () => {
+  let cardToShow = randomNumber();
+
+  removeClass();
+  cardNumber.classList.add(cardToShow.suit);
+  cardNumber.innerHTML = `<p>${cardToShow.value}</p>`;
+});
+
+//Create a function that show a random card without repeating the previous suit
+differentSuit.addEventListener("click", () => {
+  let cardToShow = randomNumber();
+
+  while (cardToShow.suit == cardNumber.classList[1]) {
+    cardToShow = randomNumber();
+  }
+
+  removeClass();
+  cardNumber.classList.add(cardToShow.suit);
+  cardNumber.innerHTML = `<p>${cardToShow.value}</p>`;
+});
+
+//Create a function that show a unique random card without repeating
+let count = 51;
+
+noRepeat.addEventListener("click", () => {
+  let newDeck = deck.filter(card => card.used === false);
+  let cardToShow = newDeck[Math.floor(Math.random() * count)];
+  console.log(count);
+
+  if (newDeck.length != 0) {
+    removeClass();
+    cardNumber.classList.add(cardToShow.suit);
+    cardNumber.innerHTML = `<p>${cardToShow.value}</p>`;
+    cardToShow.used = true;
+    count--;
+  } else {
+    console.error("fin");
+  }
+
+  console.log(newDeck);
+});
